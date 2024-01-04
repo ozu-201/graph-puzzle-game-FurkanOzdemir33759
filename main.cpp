201015586718d4a6df16fd96c14cd606c3dd8ac0
FurkanOzdemir33759 <furkan.ozdemir.33859@ozu.edu.tr>
2024-01-04 14:01:58 +0300

    unfinished dijkstra attempt

--- main.cpp
+++ main.cpp
@@ -87,10 +87,64 @@ struct Graph {
         return vector<string>{"END IS NOT REACHABLE FROM THIS START!"};
     }
 
+    // BUGGY
+    /*
+    vector<string> Dijkstra(string start, string end) {
+        int v_s = -1;
+        int v_e = -1;
+        vector<int> distance = vector<int>(vertices.size(), INT32_MAX);
+        vector<int> prev = vector<int>(vertices.size(), -1);
+        vector<int> visitedSet = vector<int>(vertices.size(), -1);
+        for (int i = 0; i < vertices.size(); i++) {
+            if (vertices[i] == start) {v_e = i;}
+            if (vertices[i] == end) {v_s = i;}
+        }
+        if (v_e == -1) {return vector<string>{"START NOT FOUND!"};}
+        if (v_s == -1) {return vector<string>{"END NOT FOUND!"};}
+        if (v_e == v_s) {return vector<string>{"SAME START AND END!"};}
+        distance[v_s] = 0;
+        bool fullyExplored = false;
+        bool firstPass = true;
+        while (!fullyExplored && visitedSet[v_e] != 1) {
+            int min = INT32_MAX;
+            int current = 0;
+            for (int i = 0; i < vertices.size(); i++) {
+                if (min > distance[i] && visitedSet[i] != 1) {
+                    current = i;
+                    min = distance[i];
+                }
+            }
+            if (min == INT32_MAX) {
+                fullyExplored = true;
+            }
+            visitedSet[current] = 1;
+            if (firstPass) {
+                prev[current] = v_s;
+                firstPass = false;
+            }
+            for (int i = 0; i < vertices.size(); i++) {
+                if (edges[current][i] == 1) {
+                    if (distance[current] + 1 < distance[i]) {
+                        distance[i] = distance[current] + 1;
+                        prev[i] = current;
+                    }
+                }
+            }
+        }
+        vector<string> path{};
+        int trace = v_e;
+        while (trace != -1) {
+            path.push_back(vertices[prev[trace]]);
+            trace = prev[trace];
+        }
+        return path;
+    }
+     */
+
     friend ostream& operator<<(ostream& os, const vector<string>& container);
 };
 
-
+// Overloaded (<<) operator to print BFS and Dijkstra results!
 ostream& operator<<(ostream& os, const vector<string>& container) {
     for (auto iter = container.begin(); iter != container.end(); iter++) {
         os << *iter;
